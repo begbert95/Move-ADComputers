d88ec42325e8c92ccb28955f929dee878d3f85a5
defenestrateddefense <begbert95@gmail.com>
2021-06-04 09:52:53 -0600

    .gitignore update

--- Move-ADComputers.ps1
+++ Move-ADComputers.ps1
@@ -1,5 +1,9 @@
 Import-Module Active Directory
 
-$JsonData = Get-Content "config.json" | ConvertFrom-Json
+$JsonData = Get-Content ".\config\config.json" | ConvertFrom-Json
 
 $Computers = Get-ADComputer -SearchBase $JsonData."search-base"
+
+foreach($Comp in $Computers){
+    
+}
